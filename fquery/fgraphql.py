26482e3f7e9df8135e9607e54f01cf19950dd1bd
Arun Sharma <asharma@fb.com>
2020-11-03 09:56:19 -0800

    Add license headers

--- fquery/fgraphql.py
+++ fquery/fgraphql.py
@@ -1,3 +1,8 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+# Copyright (c) 2016-present, Facebook, Inc. All rights reserved.
 import strawberry
 import sys
 
