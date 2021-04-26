8a74b7ab5c06d9166ce1670447bbed5d2855e30a
Arun Sharma <arun@sharma-home.net>
2021-04-26 15:33:10 -0700

    async_test: Fix deprecation warning

--- tests/async_test.py
+++ tests/async_test.py
@@ -3,11 +3,26 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 import asyncio
+import functools
+import inspect
+
+from typing import Callable
+
+
+def coroutine(fn: Callable) -> Callable:
+    if inspect.iscoroutinefunction(fn):
+        return fn
+
+    @functools.wraps(fn)
+    async def _wrapper(*args, **kwargs):
+        return fn(*args, **kwargs)
+
+    return _wrapper
 
 
 def async_test(f):
     def wrapper(*args, **kwargs):
-        coro = asyncio.coroutine(f)
+        coro = coroutine(f)
         future = coro(*args, **kwargs)
         loop = asyncio.get_event_loop()
         loop.run_until_complete(future)
