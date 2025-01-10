b7ea63fb93a461d3e3e0e6686fcec862d13ecba5
Arun Sharma <arun@sharma-home.net>
2025-01-10 09:04:13 -0800

    release: v0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fquery",
-    version="0.1",
+    version="0.2",
     description="A graph query engine",
     url="https://github.com/adsharma/fquery",
     long_description=long_description,
