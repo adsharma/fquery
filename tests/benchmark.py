09a100c46fe6a4d09d0fc74a1418058f9ef6da51
Arun Sharma <arun@sharma-home.net>
2025-01-15 13:00:21 -0800

    benchmark: add pydantic

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -2,6 +2,8 @@ import timeit
 from dataclasses import dataclass
 from datetime import datetime
 
+import pydantic
+
 from fquery.sqlmodel import model
 
 
@@ -11,8 +13,17 @@ class User:
     id: int
     name: str
     email: str
-    created_at: datetime = None
-    updated_at: datetime = None
+    created_at: datetime | None = None
+    updated_at: datetime | None = None
+
+
+@pydantic.dataclasses.dataclass
+class UserPydantic:
+    id: int
+    name: str
+    email: str
+    created_at: datetime | None = None
+    updated_at: datetime | None = None
 
 
 def create_user():
@@ -21,6 +32,10 @@ def create_user():
 
 def create_user_sqlmodel():
     return User(1, "John Doe", "john@example.com").sqlmodel()
+
+
+def create_user_pydantic():
+    return UserPydantic(1, "John Doe", "john@example.com")
 
 
 # Run the benchmark
@@ -37,6 +52,17 @@ num_iterations = 100000
 time_taken = timeit.timeit(create_user_sqlmodel, number=num_iterations)
 
 print(f"Creating {num_iterations} User SQL Model objects took {time_taken:.6f} seconds")
+print(
+    f"Average time per object creation: {(time_taken / num_iterations) * 1e9:.2f} nanoseconds"
+)
+
+# Run the benchmark
+num_iterations = 100000
+time_taken = timeit.timeit(create_user_pydantic, number=num_iterations)
+
+print(
+    f"Creating {num_iterations} User Pydantic Model objects took {time_taken:.6f} seconds"
+)
 print(
     f"Average time per object creation: {(time_taken / num_iterations) * 1e9:.2f} nanoseconds"
 )
