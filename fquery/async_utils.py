9cae7b99af26eef6356de3cd3a7d765603a13aaf
Arun Sharma <arun@sharma-home.net>
2026-05-25 15:05:47 -0700

    Handle missing asyncio event loops

--- fquery/async_utils.py
+++ fquery/async_utils.py
@@ -8,7 +8,11 @@ from contextlib import contextmanager
 
 @contextmanager
 def _get_event_loop():
-    loop = asyncio.get_event_loop()
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
     if not loop.is_running():
         yield loop
     else:
--- tests/async_test.py
+++ tests/async_test.py
@@ -23,7 +23,11 @@ def async_test(f):
     def wrapper(*args, **kwargs):
         coro = coroutine(f)
         future = coro(*args, **kwargs)
-        loop = asyncio.get_event_loop()
+        try:
+            loop = asyncio.get_running_loop()
+        except RuntimeError:
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
         loop.run_until_complete(future)
 
     return wrapper
